74003964b6f485627ab2c48a633ae86c3d64c98d
Pham Trung Dzung <Pham Trung Dzung@PTDUNG>
2021-02-22 22:54:18 +0700

    ;ll

---
+++ Testing/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class Class1
+    {
+    }
+
+    public abstract class BaseProxy : DispatchProxy
+    {
+        protected object InternalInvoke<T>(T decorated, MethodBase targetMethod, object[] args)
+        {
+            var result = targetMethod.Invoke(decorated, args);
+            if (!(result is Task resultTask)) return result;
+            if (resultTask.Exception != null)
+            {
+                throw resultTask.Exception;
+            }
+            var property = resultTask.GetType().GetTypeInfo().GetProperties()
+            .FirstOrDefault(p => p.Name == "Result");
+            if (property != null)
+            {
+                property.GetValue(resultTask);
+            }
+            return result;
+        }
+    }
+}
---
+++ Testing/Class2.cs
@@ -0,0 +1,35 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing
+{
+    class Class2
+    {
+        ////#1 
+        //var clientsManager = container.Resolve<IRedisClientsManager>();
+
+        //private static RedisClient redisClient = RedisClientManager.GetClient();
+        //public string GetValue(string key)
+        //{
+        //    var val = redisClient.Get<string>(key);
+        //    return val;
+        //}
+        ////#2 
+        //public string GetValue(string key, RedisClient redisClient)
+        //{
+        //    var val = redisClient.Get<string>(key);
+        //    return val;
+        //}
+        ////#3 
+        //public string GetValue(string key)
+        //{
+        //    using (var redisClient = RedisClientManager.GetClient())
+        //    {
+        //        var val = redisClient.Get<string>(key);
+        //        return val;
+        //    }
+        //}
+    }
+}
